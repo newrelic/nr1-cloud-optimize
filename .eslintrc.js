newrelic/nr1-cloud-optimize

commit 32598611e8b9f88d392a29ebbaf953f1bca12781

chore: update ESLint (#40)
* chore: update ESLint

newrelic#35

* chore: run eslint fix

* fix: eslint issues.

* fix: adjust proptypes and instances of return ''

Co-authored-by: Joel Worrall <jworrall@newrelic.com>

.eslintrc.js
@@ -1,29 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "prettier",
-        "prettier/react",
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "prettier"
-    ],
-    "rules": {
-        "prettier/prettier": "error"
-    }
+  "env": {
+      "browser": true,
+      "es6": true
+  },
+  "extends": [
+      'plugin:@newrelic/eslint-plugin-newrelic/react',
+      'plugin:@newrelic/eslint-plugin-newrelic/jest',
+      'plugin:@newrelic/eslint-plugin-newrelic/prettier'
+  ],
+  "globals": {
+      "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly"
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react",
+      "prettier"
+  ],
+  "rules": {
+      "prettier/prettier": "error"
+  }
 };

nerdlets/cloud-optimize-nerdlet/components/accountCards.js
@@ -1,91 +1,198 @@
-import React from 'react'
-import { Card, Table, Icon, Dimmer, Loader, Segment } from 'semantic-ui-react'
-import OptimizationCandidates from './optimizationCandidates'
-import SnapshotCard from './snapshots/snapshotCard'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Table, Icon, Dimmer, Loader, Segment } from 'semantic-ui-react';
+import OptimizationCandidates from './optimizationCandidates';
+import SnapshotCard from './snapshots/snapshotCard';
 
 export default class AccountCards extends React.Component {
+  static propTypes = {
+    loading: PropTypes.bool,
+    completedAccounts: PropTypes.number,
+    accounts: PropTypes.number,
+    instances: PropTypes.number,
+    sorted: PropTypes.array,
+    config: PropTypes.object,
+    fetchSnapshots: PropTypes.func,
+    cloudOptimizeSnapshots: PropTypes.func,
+    height: PropTypes.number,
+    groupByDefault: PropTypes.string,
+    snapshots: PropTypes.array
+  };
 
-    render() {
-        let isLoading = this.props.loading || this.props.completedAccounts != this.props.accounts
-        let noAccounts = isLoading == false && this.props.accounts == 0
+  render() {
+    const isLoading =
+      this.props.loading ||
+      this.props.completedAccounts !== this.props.accounts;
+    const noAccounts = isLoading === false && this.props.accounts === 0;
 
-        return (
-            <Segment className="segment-clear" style={{textAlign:"center", marginTop:"0px", minHeight:this.props.height}}>
-                <Dimmer active={isLoading}>
-                    <Loader style={{top:"150px"}} size="big">
-                        Please be patient while we analyze your Accounts and Instances... <br/><br/>
-                        {this.props.completedAccounts} of {this.props.accounts} Accounts Inspected <br/><br/>
-                        {this.props.instances} Instances Inspected
-                    </Loader>
-                </Dimmer>
-                
-                <h2 style={{ display: noAccounts ? "" : "none" }}>
-                    Unable to load accounts!
-                </h2>
-                <h2 style={{ display: noAccounts ? "" : "none" }}>
-                    Please refresh to try again, and ensure you are using a UUID associated to your account.
-                </h2>
+    return (
+      <Segment
+        className="segment-clear"
+        style={{
+          textAlign: 'center',
+          marginTop: '0px',
+          minHeight: this.props.height
+        }}
+      >
+        <Dimmer active={isLoading}>
+          <Loader style={{ top: '150px' }} size="big">
+            Please be patient while we analyze your Accounts and Instances...{' '}
+            <br />
+            <br />
+            {this.props.completedAccounts} of {this.props.accounts} Accounts
+            Inspected <br />
+            <br />
+            {this.props.instances} Instances Inspected
+          </Loader>
+        </Dimmer>
 
-                <Card.Group style={{margin:"auto","width":"100%"}} centered>
-                    {this.props.sorted.map((item, i)=>{
-                        let header = item.group
-                        if(this.props.config.groupBy == "apmApplicationNames"){
-                            let apps = item.group ? item.group.split("|").filter(Boolean) : "uncategorized"
-                            header = apps.map((app,i)=>i+1 == apps.length ? app : app + ", ")
-                        }
+        <h2 style={{ display: noAccounts ? '' : 'none' }}>
+          Unable to load accounts!
+        </h2>
+        <h2 style={{ display: noAccounts ? '' : 'none' }}>
+          Please refresh to try again, and ensure you are using a UUID
+          associated to your account.
+        </h2>
 
-                        header = (header == "null" || !header) ? "uncategorized" : header
+        <Card.Group style={{ margin: 'auto', width: '100%' }} centered>
+          {this.props.sorted.map((item, i) => {
+            let header = item.group;
+            if (this.props.config.groupBy === 'apmApplicationNames') {
+              const apps = item.group
+                ? item.group.split('|').filter(Boolean)
+                : 'uncategorized';
+              header = apps.map((app, i) =>
+                i + 1 === apps.length ? app : `${app}, `
+              );
+            }
 
-                        if((this.props.config.groupBy || this.props.groupByDefault) == "accountName"){
-                            header = <a className="card-content-light" href={`https://rpm.newrelic.com/accounts/${item.instances[0].accountId}`} rel="noopener noreferrer" target="_blank">{header} <Icon name='external alternate' /></a>
-                        }
+            header = header === 'null' || !header ? 'uncategorized' : header;
 
-                        return (
-                            <Card key={i} className="card card-light" color="green" style={{minWidth:"350px"}}>
-                            <Card.Content className="card-content-light">
-                                <span style={{fontSize:"13px"}}>{header}</span>
-                                <span style={{float:"right", cursor:"pointer"}}><SnapshotCard fetchSnapshots={this.props.fetchSnapshots} config={this.props.config} data={item} snapshots={this.props.snapshots} cloudOptimizeSnapshots={this.props.cloudOptimizeSnapshots}/></span>
-                            </Card.Content>
-                            <Card.Content style={{paddingTop:"5px", paddingBottom:"5px"}}>
-                            <Table celled inverted={false} basic='very'>
-                                <Table.Header>
-                                    <Table.Row>
-                                        <Table.HeaderCell></Table.HeaderCell>
-                                        <Table.HeaderCell>Monthly</Table.HeaderCell>
-                                        <Table.HeaderCell>Yearly</Table.HeaderCell>
-                                    </Table.Row>
-                                    </Table.Header>
-                                    <Table.Body>
-                                    <Table.Row>
-                                        <Table.Cell>Non-Optimized</Table.Cell>
-                                        <Table.Cell>${(item.nonOptimizedCost*720).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</Table.Cell>
-                                        <Table.Cell>${(item.nonOptimizedCost*720*12).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</Table.Cell>
-                                    </Table.Row>
-                                    <Table.Row>
-                                        <Table.Cell>Optimized</Table.Cell>
-                                        <Table.Cell>${(item.optimizedCost*720).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</Table.Cell>
-                                        <Table.Cell>${(item.optimizedCost*720*12).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</Table.Cell>
-                                    </Table.Row>
-                                    <Table.Row>
-                                        <Table.Cell positive>Saving</Table.Cell>
-                                        <Table.Cell positive>${(item.saving*720).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</Table.Cell>
-                                        <Table.Cell positive>${(item.saving*720*12).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</Table.Cell>
-                                    </Table.Row>
-                                    </Table.Body>
-                                    <Table.Footer>
-                                    <Table.Row>
-                                        <Table.HeaderCell style={{textAlign:"right"}} colSpan={3}>{item.nonOptimizedCount} of {item.totalInstances} Instances to Optimize | {item.nonOptimizedCost > 0 ? ((Math.abs(item.saving)/(item.nonOptimizedCost)) * 100).toFixed(2) : 0}% Cost Saving</Table.HeaderCell>
-                                    </Table.Row>
-                                    </Table.Footer>
-                                </Table>
-                            </Card.Content>
-                            <Card.Content>
-                                <OptimizationCandidates instances={item.instances} header={header} />
-                            </Card.Content>
-                        </Card>)
-                    })}
-                </Card.Group>
-            </Segment>
-        )
-    }
+            if (
+              (this.props.config.groupBy || this.props.groupByDefault) ===
+              'accountName'
+            ) {
+              header = (
+                <a
+                  className="card-content-light"
+                  href={`https://rpm.newrelic.com/accounts/${item.instances[0].accountId}`}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  {header} <Icon name="external alternate" />
+                </a>
+              );
+            }
+
+            return (
+              <Card
+                key={i}
+                className="card card-light"
+                color="green"
+                style={{ minWidth: '350px' }}
+              >
+                <Card.Content className="card-content-light">
+                  <span style={{ fontSize: '13px' }}>{header}</span>
+                  <span style={{ float: 'right', cursor: 'pointer' }}>
+                    <SnapshotCard
+                      fetchSnapshots={this.props.fetchSnapshots}
+                      config={this.props.config}
+                      data={item}
+                      snapshots={this.props.snapshots}
+                      cloudOptimizeSnapshots={this.props.cloudOptimizeSnapshots}
+                    />
+                  </span>
+                </Card.Content>
+                <Card.Content
+                  style={{ paddingTop: '5px', paddingBottom: '5px' }}
+                >
+                  <Table celled inverted={false} basic="very">
+                    <Table.Header>
+                      <Table.Row>
+                        <Table.HeaderCell />
+                        <Table.HeaderCell>Monthly</Table.HeaderCell>
+                        <Table.HeaderCell>Yearly</Table.HeaderCell>
+                      </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                      <Table.Row>
+                        <Table.Cell>Non-Optimized</Table.Cell>
+                        <Table.Cell>
+                          $
+                          {(item.nonOptimizedCost * 720)
+                            .toFixed(2)
+                            .replace(/\d(?=(\d{3})+\.)/g, '$&,')}
+                        </Table.Cell>
+                        <Table.Cell>
+                          $
+                          {(item.nonOptimizedCost * 720 * 12)
+                            .toFixed(2)
+                            .replace(/\d(?=(\d{3})+\.)/g, '$&,')}
+                        </Table.Cell>
+                      </Table.Row>
+                      <Table.Row>
+                        <Table.Cell>Optimized</Table.Cell>
+                        <Table.Cell>
+                          $
+                          {(item.optimizedCost * 720)
+                            .toFixed(2)
+                            .replace(/\d(?=(\d{3})+\.)/g, '$&,')}
+                        </Table.Cell>
+                        <Table.Cell>
+                          $
+                          {(item.optimizedCost * 720 * 12)
+                            .toFixed(2)
+                            .replace(/\d(?=(\d{3})+\.)/g, '$&,')}
+                        </Table.Cell>
+                      </Table.Row>
+                      <Table.Row>
+                        <Table.Cell positive>Saving</Table.Cell>
+                        <Table.Cell positive>
+                          $
+                          {(item.saving * 720)
+                            .toFixed(2)
+                            .replace(/\d(?=(\d{3})+\.)/g, '$&,')}
+                        </Table.Cell>
+                        <Table.Cell positive>
+                          $
+                          {(item.saving * 720 * 12)
+                            .toFixed(2)
+                            .replace(/\d(?=(\d{3})+\.)/g, '$&,')}
+                        </Table.Cell>
+                      </Table.Row>
+                    </Table.Body>
+                    <Table.Footer>
+                      <Table.Row>
+                        <Table.HeaderCell
+                          style={{ textAlign: 'right' }}
+                          colSpan={3}
+                        >
+                          {item.nonOptimizedCount} of {item.totalInstances}{' '}
+                          Instances to Optimize |{' '}
+                          {item.nonOptimizedCost > 0
+                            ? (
+                                (Math.abs(item.saving) /
+                                  item.nonOptimizedCost) *
+                                100
+                              ).toFixed(2)
+                            : 0}
+                          % Cost Saving
+                        </Table.HeaderCell>
+                      </Table.Row>
+                    </Table.Footer>
+                  </Table>
+                </Card.Content>
+                <Card.Content>
+                  <OptimizationCandidates
+                    instances={item.instances}
+                    header={header}
+                  />
+                </Card.Content>
+              </Card>
+            );
+          })}
+        </Card.Group>
+      </Segment>
+    );
+  }
 }

commit 1c4afc996964417a900255883c29ce1c65be1cba

fix: config & region update (issues caused by #40)

.eslintrc.js
@@ -1,30 +1,28 @@
 module.exports = {
-  "env": {
-      "browser": true,
-      "es6": true
+  env: {
+    browser: true,
+    es6: true,
   },
-  "extends": [
-      'plugin:@newrelic/eslint-plugin-newrelic/react',
-      'plugin:@newrelic/eslint-plugin-newrelic/jest',
-      'plugin:@newrelic/eslint-plugin-newrelic/prettier'
+  extends: [
+    'plugin:@newrelic/eslint-plugin-newrelic/react',
+    'plugin:@newrelic/eslint-plugin-newrelic/jest',
+    'plugin:@newrelic/eslint-plugin-newrelic/prettier',
   ],
-  "globals": {
-      "Atomics": "readonly",
-      "SharedArrayBuffer": "readonly"
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  "parser": "babel-eslint",
-  "parserOptions": {
-      "ecmaFeatures": {
-          "jsx": true
-      },
-      "ecmaVersion": 2018,
-      "sourceType": "module"
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  "plugins": [
-      "react",
-      "prettier"
-  ],
-  "rules": {
-      "prettier/prettier": "error"
-  }
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 0,
+  },
 };

.prettierrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    singleQuote: true,
-    trailingComma: "es5",
+  singleQuote: true,
+  trailingComma: 'none',
 };

nerdlets/cloud-optimize-apm/components/optimization-candidates.js
@@ -293,7 +293,6 @@ export default class OptimizationCandidates extends React.Component {
                             }
                           >
                             {instance.hostname}{' '}
-                            <Icon name="external alternate" />
                           </Table.Cell>
                           <Table.Cell>
                             {instance.maxCpuPercent.toFixed(2)}

nerdlets/cloud-optimize-nerdlet/components/accountCards.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Table, Icon, Dimmer, Loader, Segment } from 'semantic-ui-react';
+import { Card, Table, Dimmer, Loader, Segment } from 'semantic-ui-react';
 import OptimizationCandidates from './optimizationCandidates';
 import SnapshotCard from './snapshots/snapshotCard';
 
@@ -79,7 +79,7 @@ export default class AccountCards extends React.Component {
                   rel="noopener noreferrer"
                   target="_blank"
                 >
-                  {header} <Icon name="external alternate" />
+                  {header}
                 </a>
               );
             }

nerdlets/cloud-optimize-nerdlet/components/optimizationCandidates.js
@@ -269,7 +269,7 @@ export default class OptimizationCandidates extends React.Component {
                           rel="noopener noreferrer"
                           target="_blank"
                         >
-                          {instance.hostname} <Icon name="external alternate" />
+                          {instance.hostname}
                         </a>
                       </Table.Cell>
                       <Table.Cell>
@@ -285,7 +285,6 @@ export default class OptimizationCandidates extends React.Component {
                           target="_blank"
                         >
                           {instance.entityName}{' '}
-                          <Icon name="external alternate" />
                         </a>
                       </Table.Cell>
                       <Table.Cell>

nerdlets/shared/components/config.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {
   Divider,
   Form,
@@ -12,13 +11,7 @@ import {
 } from 'semantic-ui-react';
 import { deleteDocument } from '../lib/utils';
 
-export default class Config extends React.PureComponent {
-  static propTypes = {
-    config: PropTypes.object,
-    handleParentState: PropTypes.func,
-    button: PropTypes.bool
-  };
-
+export default class Config extends React.Component {
   constructor(props) {
     super(props);
     this.handleConfigurator = this.handleConfigurator.bind(this);
@@ -81,6 +74,7 @@ export default class Config extends React.PureComponent {
             </Menu.Item>
           )
         }
+        closeIcon
       >
         <Modal.Header>Advanced Configuration</Modal.Header>
         <Modal.Content className="config config-dark">

nerdlets/shared/components/pricingSelector.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Dropdown, Modal, Menu, Form, Button } from 'semantic-ui-react';
 
-export default class PricingSelector extends React.PureComponent {
-  static propTypes = {
-    config: PropTypes.object,
-    fetchCloudPricing: PropTypes.func,
-    handleParentState: PropTypes.func,
-    cloudRegions: PropTypes.object,
-    button: PropTypes.bool
-  };
-
+export default class PricingSelector extends React.Component {
   constructor(props) {
     super(props);
     this.handleConfigurator = this.handleConfigurator.bind(this);
@@ -55,6 +46,7 @@ export default class PricingSelector extends React.PureComponent {
             <Menu.Item>Set Cloud Regions</Menu.Item>
           )
         }
+        closeIcon
       >
         <Modal.Header>Configure Regions</Modal.Header>
         <Modal.Content>