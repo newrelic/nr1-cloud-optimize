44972dfdb4c9cc22bc10917163800c738f548113
Kav91 <kav.pather@gmail.com>
2022-04-25 17:34:34 +1000

    fix: eslint

--- nerdlets/shared/components/loader/cssLoader.js
+++ nerdlets/shared/components/loader/cssLoader.js
@@ -1,35 +1,35 @@
-import React from "react";
+import React from 'react';
 
 export default class CssLoader extends React.Component {
   render() {
     const { loader } = this.props;
 
-    if (loader === "lds-ripple") {
+    if (loader === 'lds-ripple') {
       return (
         <div className="lds-ripple">
-          <div></div>
-          <div></div>
+          <div />
+          <div />
         </div>
       );
     }
 
-    if (!loader || loader === "lds-ring") {
+    if (!loader || loader === 'lds-ring') {
       return (
         <div className="lds-ring">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div />
+          <div />
+          <div />
+          <div />
         </div>
       );
     }
 
-    if (!loader || loader === "lds-facebook") {
+    if (!loader || loader === 'lds-facebook') {
       return (
         <div className="lds-facebook">
-          <div></div>
-          <div></div>
-          <div></div>
+          <div />
+          <div />
+          <div />
         </div>
       );
     }
